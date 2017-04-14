38a0cf3c30b738a669bba6d6a24a07d9a7354eea
oscar luna <os.lun11@gmail.com>
2017-04-14 11:53:19 -0700

    Database Added and Working on Bamazon Customer File

---
+++ bamazonCustomer.js
@@ -0,0 +1,47 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: '',
+		database: 'bamazonDB',
+});
+
+connection.connect(function(err) {
+		if(err) throw err;
+		commence();
+});
+
+
+var commence = function() {
+		connection.query('SELECT * FROM bamazonProducts', function(err, res) {
+				for (var i=0; i<res.length; i++) {
+						if (res[i] != 0) {
+							console.log('ID: ' + res[i].item_id + ' | ' + 'Product: ' + res[i].product_name + ' | ' + 'Price: ' + '$' + res[i].price);
+						}
+
+				}
+
+				shopProduct();
+		})
+};
+
+
+var shopProduct = function(res) {
+		inquirer.promt([{
+				name: 'product_id',
+				type: 'input',
+				message: 'What product would you like to purchase?',
+				required: true
+		},{
+				name: 'product_quantity',
+				type: 'input',
+				message: 'What is your desired product quantity that you would like to purchase?',
+				required: true
+		}]).then(function(answer) {
+				console.log(answer);
+		})
+		    
+};
---
+++ bamazonProducts.sql
@@ -0,0 +1,36 @@
+
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE bamazonProducts (
+	'item_id' INT auto_increment NOT NULL,
+'product_name' VARCHAR(100) NOT NULL,
+'department_name' VARCHAR(100) NOT NULL,
+'price' DECIMAL(10,4) NOT NULL,
+'stock_quantity' INT NULL,
+PRIMARY KEY (item_id)
+);
+
+INSERT INTO bamazonProducts (product_name, department_name, price, stock_quantity)
+VALUES ('Peanut Butter', 'food', '2.25', 40),
+('Taco', 'Food', '1.15', 100),
+('Hair Gel', 'Beuty', '4.10', 40),
+('I-Phone 7', 'Electronics', '600', 15),
+('Sun Glasses', 'Fashion', '125', 40),
+('Macbook Pro', 'Electronics', '1800.75', 61),
+('Diapers', 'Toddlers', '12.00', 55),
+('Hot Sauce', 'Food', '3.72', 90),
+('Sushi Hand Roll', 'Food', '5.55', 150),
+('Reddit Gold', 'Online', '3.99', 10000),
+('Dog Food', 'Pets', '20.99', 50);
+
+
+SELECT * FROM bamazonProducts;
+
+CREATE TABLE bamazonDepartments (
+	'department_id' INT auto_increment PRIMARY KEY,
+'department_name' VARCHAR(100) NOT NULL,
+'overhead_cost' DECIMAL(10, 4),
+'total_sales' DECIMAL(10, 4)
+);
